76a2dd950bf5a993571e652342011c663af26720
Samarth Cham <64300725+Samarth-Cham@users.noreply.github.com>
2024-08-14 14:20:56 +0530

    Add files via upload
    Master file uploaded

---
+++ ShrubHub2-master/config/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getFirestore, collection } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYiqLGBhtIf3P5Y2kv4UCJJJrj3MCVkwk",
+  authDomain: "shrub-hub-final.firebaseapp.com",
+  projectId: "shrub-hub-final",
+  storageBucket: "shrub-hub-final.appspot.com",
+  messagingSenderId: "821424810922",
+  appId: "1:821424810922:web:88592f2b527d4595c919d3"
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+})
+
+export const db = getFirestore(app);
+
+export const usersRef = collection(db,'users');
